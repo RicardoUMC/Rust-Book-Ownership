04273453ae8a9203173df137d9c06024a8e633ad
Ricardo Mora <rurimoca@gmail.com>
2025-03-10 04:38:29 -0600

    Added Ownership functions (return values)

--- src/main.rs
+++ src/main.rs
@@ -56,4 +56,64 @@ fn main() {
     // data).
     println!("x = {x}, y = {y}");
 
+    //======> Ownership and Functions
+
+    // Passing a value to a funtion will move or copy the variable, depending on whether
+    // the data type implements the Copy trait.
+    let s = String::from("hello");
+    takes_ownership(s); //<------ Here, s's value is moved into the function...
+
+    let x = 5;
+    makes_copy(x); //<----------- Here, x's value is copied.
+    println!("{}", x); //<------- So it is safe to use x afterward.
+
+    //======> Return Values and Scope 
+
+    // We can also transfer ownership by returning values.
+    let s1 = gives_ownership(); // <----------- gives_ownership moves its return.
+    let s2 = String::from("hello");
+    let s3 = takes_and_gives_back(s2); // <---- s2 is moved into
+                                       // takes_and_gives_back, which also
+                                       // moves its return value into s3.
+
+    // Before seeing what are the `references` we can use tuples for returning a
+    // necessary value which we passed in to a function.
+    let s1 = String::from("hello");
+
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{s2}' is {len}.");
+}
+
+/***************************/
+/* Ownership and Functions */
+/***************************/
+
+fn takes_ownership(some_string: String) {
+    println!("{some_string}")
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{some_integer}");
+}
+
+/***************************/
+/* Return Values and Scope */
+/***************************/
+
+fn gives_ownership() -> String {
+    let some_string = String::from("yours");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
+}
+
+// This is an option: To pass back a value we want to use it again. Sadly, not all 
+// times we might want to transfer ownerships.
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+
+    (s, length)
 }
